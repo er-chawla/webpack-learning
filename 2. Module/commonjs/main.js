64729c1223fdc0e7f59f517fe3ef4903fe9c1bbc
er-chawla <er.manu27@gmail.com>
2025-10-25 17:57:15 +0530

    Webpack imp for modules es6/ commonjs

---
+++ 2. Module/commonjs/main.js
@@ -0,0 +1,5 @@
+const add = require('./functions/add');
+
+const output = add(1, 2);
+
+console.log(output);
---
+++ 2. Module/es6/main.js
@@ -0,0 +1,8 @@
+import * as $ from './functions';
+
+const addOutput = $.add(1, 2);
+const subOutput = $.subtract(1, 2);
+
+console.log(addOutput);
+
+console.log(subOutput);
