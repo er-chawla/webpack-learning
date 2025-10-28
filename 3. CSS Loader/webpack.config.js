cf2cf94a671bbd6bfca92b67906f77df78453cf3
er-chawla <er.manu27@gmail.com>
2025-10-28 17:47:39 +0530

    CSS loader implementation

---
+++ 3. CSS Loader/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader', options: { modules: true } },
+                ],
+            },
+        ],
+    },
+};
